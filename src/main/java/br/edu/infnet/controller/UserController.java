a057efefb3f8ac55144f6e73fab951b4ec07350e
Wellington Nascimento <wnsferreira@gmail.com>
2021-08-09 11:04:15 -0300

    servlet

---
+++ src/main/java/br/edu/infnet/controller/UserController.java
@@ -0,0 +1,32 @@
+package br.edu.infnet.controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+public class UserController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    
+    public UserController() {
+        super();
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+	}
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		String nome = request.getParameter("nome");
+		String email = request.getParameter("email");
+		String senha = request.getParameter("senha");
+		
+		System.out.printf("Usuário: %s email: %s senha: %s \n", nome, email, senha);
+	}
+
+}
